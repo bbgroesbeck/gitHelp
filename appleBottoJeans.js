cea0ff1948b990137421fd46790ee9ebce16c206
Jordan <jordan@jordans-air.lan>
2023-01-03 18:56:08 -0700

    I regret nothing!

---
+++ appleBottoJeans.js
@@ -0,0 +1,7 @@
+function appleBottomJeans (boots) {
+    if (boots === 'with the fur') {
+        return "the whole clube be looking at her"
+    } else if (boots === 'Reboks with the straps') {
+        return "them baggy sweat pants"
+    } else return "everybody get lo!"
+}
