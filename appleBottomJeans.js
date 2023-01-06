b9d039561a496b299bcd3fcc5f394e12be83d699
Jordan <jordan@Jordans-MacBook-Air.local>
2023-01-06 08:13:19 -0700

    made changes

--- appleBottomJeans.js
+++ appleBottomJeans.js
@@ -11,15 +11,15 @@ function bananas (shiz) {
     shiz === 'bananas' ? 'b.a.n.a.n.a.s' : 'Please try again.'
 }
 
+
+
+function bananas (shiz) {
+    shiz === 'bananas' ? 'b.a.n.a.n.a.s' : 'Please try again.'
+}
+
 // My frozen dairy based confections do indeed
 // create a focal point to which young males
 // of the species are drawn.
 // 
 // I could attempt to educate you on this subject
 // but it would require monetary recompense
-    } else return "everybody get low low low low low low!"
-}
-
-function bananas (shiz) {
-    shiz === 'bananas' ? 'b.a.n.a.n.a.s' : 'Please try again.'
-}
