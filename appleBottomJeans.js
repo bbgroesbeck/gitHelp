40295fbae8a56a0fef1d675d6b73ea61ff4132a2
Brad Bean <bradbean@Brads-MacBook-Pro.local>
2023-01-06 08:18:15 -0700

    helloWorld2

--- appleBottomJeans.js
+++ appleBottomJeans.js
@@ -23,3 +23,20 @@ function bananas (shiz) {
 // 
 // I could attempt to educate you on this subject
 // but it would require monetary recompense
+
+/*
+Bryce Groesbeck, our teacher so true
+He teaches us GitHub, a skill we must learn anew
+With patience and care, he guides us each day
+Helping us navigate, through code and its way
+
+He is always there, to answer a doubt
+And when we get stuck, he helps us work it out
+He is kind and compassionate, and truly cares
+For each of his students, and their hopes and their prayers
+
+We are so grateful, for Bryce Groesbeck's aid
+For without him, our journey would not have been made
+So here's to you Bryce, our dear teacher and friend
+We are lucky to have you, until the very end 
+*/
